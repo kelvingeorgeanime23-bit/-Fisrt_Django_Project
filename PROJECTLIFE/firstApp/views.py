a2623a046e4fb79ec227e4255e5fc492b6fcc418
Kelvin George <kelvingeorge.anime23@gamil.com>
2026-03-05 16:22:44 +0300

    Ginger templating

--- PROJECTLIFE/firstApp/views.py
+++ PROJECTLIFE/firstApp/views.py
@@ -4,8 +4,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(response):
-    return render(response, 'main.html')
+    context = {"data": "WELCOME TO THE HOME PAGE"}
+    return render(response, 'home.html', context)
 
 def about(response):
-    return render(response, 'about.html')
+    context = {"data": "WELCOME TO THE ABOUT PAGE"}
+    return render(response, 'about.html', context)
+
+def contact(response):
+    context = {"data": "WELCOME TO THE CONTACT PAGE"}
+    return render(response, 'contact.html', context)
+
+
+    
 
